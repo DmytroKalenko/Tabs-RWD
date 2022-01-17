b44eff4908b716e69f712ac91909b036c3de7b6d
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2022-01-17 21:54:23 +0100

    redesign

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
-function setProparty() {
-    let tabs = {
-        ".title1": '.content1',
-        ".title2": '.content2',
-        ".title3": '.content3',
-        ".title4": '.content4'
+// create tabs
+
+    const tabsTitles = document.querySelectorAll('.tabs .tabs__titles .title_item');
+    const itemsContent = document.querySelectorAll('.tabs .content_items .content_item');
+
+    const mobile = window.innerWidth <= 978;
+
+    if (!mobile) {
+        tabsTitles[0].classList.add('active');
+        itemsContent[0].classList.add('active');
     }
 
-    const productInfoItems = Object.keys(tabs);
-    const productInfo__Content = Object.values(tabs)
 
-    for (let i = 0; i < productInfoItems.length; i++) {
-        document.querySelector(productInfo__Content[i]).classList.remove('Active');
-        document.querySelector(productInfoItems[i]).addEventListener('click', function() {
-            for (let k = 0; k < productInfoItems.length; k++) {
-                document.querySelector(productInfoItems[k]).classList.remove('Active');
-                document.querySelector(productInfo__Content[k]).style.display = 'none';
-                document.querySelector(productInfo__Content[k]).classList.remove('ActiveContent');
-                if (window.innerWidth <= 978) {
-                    document.querySelector('.tabs__title').insertBefore(document.querySelector(productInfo__Content[i]), document.querySelector(productInfoItems[i]).nextElementSibling)
+    for (let i = 0; i < tabsTitles.length; i++) {
+        tabsTitles[i].addEventListener('click', function () {
+
+            if (mobile != true) {
+                for (let k = 0; k < itemsContent.length; k++) {
+                    tabsTitles[k].classList.remove('active');
+                    itemsContent[k].classList.remove('active');
                 }
+                this.classList.add('active');
+                itemsContent[i].classList.add('active');
+            }else{
+                this.classList.toggle('active');
+                this.nextElementSibling.classList.toggle('active');
             }
-            this.classList.add('Active');
-            document.querySelector(productInfo__Content[i]).style.display = 'block';
-            document.querySelector(productInfo__Content[i]).classList.add('ActiveContent');
-        })
-    }
-}
 
-setProparty()
+        })
+        if (mobile) {
+            document.querySelector('.tabs__titles').insertBefore(itemsContent[i], tabsTitles[i].nextElementSibling);
+        }
+    }
+
